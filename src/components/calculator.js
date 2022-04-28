8050271f5ae4f68c0ab62be7d69c077976d07922
AnicetFantomas <94958024+AnicetFantomas@users.noreply.github.com>
2022-04-28 15:48:44 +0200

    Update add operate and calculate files

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,64 +1,69 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './calculator.css';
 import calculate from '../logic/calculate';
 
-const Calculator = () => {
-  const [initialCalc, setCalc] = useState({
-    next: '',
-    total: 0,
-    operation: '',
-  });
-  const { next, total, operation } = initialCalc;
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      next: '',
+      total: 0,
+      operation: '',
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
 
-  const handleClick = (e) => {
-    e.preventDefault();
-    setCalc((state) => calculate({
+  handleClick(e) {
+    this.setState((state) => calculate({
       next: state.next,
       total: state.total,
       operation: state.operation,
     }, e.target.innerHTML));
-  };
+  }
 
-  return (
-    <section>
-      <div className="container">
-        <div className="display">
-          <div className="display-result">
-            {total}
-            {' '}
-            {operation}
-            {' '}
-            {next}
+  render() {
+    const { next, total, operation } = this.state;
+    return (
+      <section>
+        <div className="container">
+          <div className="display">
+            <div className="display-result">
+              {total}
+              {' '}
+              {operation}
+              {' '}
+              {next}
+            </div>
+          </div>
+          <div className="all-buttons">
+            <button type="button" className="button clear-all" onClick={this.handleClick}>AC</button>
+            <button type="button" className="button last-entity-clear" onClick={this.handleClick}>+/-</button>
+            <button type="button" className="button operation" onClick={this.handleClick}>%</button>
+            <button type="button" className="button operation side-col" onClick={this.handleClick}>÷</button>
+
+            <button type="button" className="button number" onClick={this.handleClick}>7</button>
+            <button type="button" className="button number" onClick={this.handleClick}>8</button>
+            <button type="button" className="button number" onClick={this.handleClick}>9</button>
+            <button type="button" className="button operation side-col" onClick={this.handleClick}>x</button>
+
+            <button type="button" className="button number" onClick={this.handleClick}>4</button>
+            <button type="button" className="button number" onClick={this.handleClick}>5</button>
+            <button type="button" className="button number" onClick={this.handleClick}>6</button>
+            <button type="button" className="button operation side-col" onClick={this.handleClick}>-</button>
+
+            <button type="button" className="button number" onClick={this.handleClick}>1</button>
+            <button type="button" className="button number" onClick={this.handleClick}>2</button>
+            <button type="button" className="button number" onClick={this.handleClick}>3</button>
+            <button type="button" className="button operation side-col" onClick={this.handleClick}>+</button>
+
+            <button type="button" className="button number btn-0" onClick={this.handleClick}>0</button>
+            <button type="button" className="button number" onClick={this.handleClick}>.</button>
+            <button type="button" className="button equal side-col" onClick={this.handleClick}>=</button>
           </div>
         </div>
-        <div className="all-buttons">
-          <button type="button" className="button clear-all" onClick={handleClick}>AC</button>
-          <button type="button" className="button last-entity-clear" onClick={handleClick}>+/-</button>
-          <button type="button" className="button operation" onClick={handleClick}>%</button>
-          <button type="button" className="button operation side-col" onClick={handleClick}>÷</button>
-
-          <button type="button" className="button number" onClick={handleClick}>7</button>
-          <button type="button" className="button number" onClick={handleClick}>8</button>
-          <button type="button" className="button number" onClick={handleClick}>9</button>
-          <button type="button" className="button operation side-col" onClick={handleClick}>x</button>
-
-          <button type="button" className="button number" onClick={handleClick}>4</button>
-          <button type="button" className="button number" onClick={handleClick}>5</button>
-          <button type="button" className="button number" onClick={handleClick}>6</button>
-          <button type="button" className="button operation side-col" onClick={handleClick}>-</button>
-
-          <button type="button" className="button number" onClick={handleClick}>1</button>
-          <button type="button" className="button number" onClick={handleClick}>2</button>
-          <button type="button" className="button number" onClick={handleClick}>3</button>
-          <button type="button" className="button operation side-col" onClick={handleClick}>+</button>
-
-          <button type="button" className="button number btn-0" onClick={handleClick}>0</button>
-          <button type="button" className="button number" onClick={handleClick}>.</button>
-          <button type="button" className="button equal side-col" onClick={handleClick}>=</button>
-        </div>
-      </div>
-    </section>
-  );
-};
+      </section>
+    );
+  }
+}
 
 export default Calculator;
